96c3e55896df5230ed898f92cae8bf963b93010b
Prajwal238 <prajwal_katakam@srmap.edu.in>
2025-06-08 16:26:17 +0530

    Changes to fix error while doing AI powered search for influencers.

--- src/services/influencerService.js
+++ src/services/influencerService.js
@@ -54,6 +54,7 @@ InfluencerService.prototype.getInfluencersByCampaignId = async function(userId, 
 InfluencerService.prototype.getInfluencersForCampaignFromLLM = async function(userId, campaignId, userPrompt) {
     const campaign = await campaignModel.getCampaignById(campaignId);
 
+    userPrompt = userPrompt || `I need to find the top 5 best influencers for my campaign ${campaign.campaignName} whose objective is ${campaign.objective}`;
     const filters = await influencerAgent.getFiltersFromUserPromptForRetrieval(userPrompt);
     const response = await axios({
         method: 'POST',
@@ -62,7 +63,7 @@ InfluencerService.prototype.getInfluencersForCampaignFromLLM = async function(us
           'Content-Type': 'application/json',
         },
         data: {
-          userPrompt: userPrompt || `I need to find the top 5 best influencers for my campaign ${campaign.campaignName} whose objective is ${campaign.objective}`,
+          userPrompt: userPrompt,
           filters: filters
         },
     });
